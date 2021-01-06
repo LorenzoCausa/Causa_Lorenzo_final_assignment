d0235a567b3edf20835e22ba8fe419da4bcb53f3
Lorenzo Causa ID4519089 <lorenzo.causa@libero.it>
2021-01-06 10:27:24 +0000

    first commit

---
+++ doc/html/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['random_5fservice_2ecpp',['random_service.cpp',['../random__service_8cpp.html',1,'']]]
+];
---
+++ doc/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['current_5fposition',['current_position',['../namespaceinterface.html#a13ab2be968e67ac3d7d52e5e25c430c3',1,'interface']]]
+];
